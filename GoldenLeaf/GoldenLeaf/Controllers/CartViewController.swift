484fa167587635047d5295514fe84c6e72fe21cf
ShathaAljuaid <aljuaidshatha@gmail.com>
2023-01-15 21:11:36 +0300

    Cart layout

--- GoldenLeaf/GoldenLeaf/Controllers/CartViewController.swift
+++ GoldenLeaf/GoldenLeaf/Controllers/CartViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +18,19 @@ class CartViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
+
+extension CartViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
