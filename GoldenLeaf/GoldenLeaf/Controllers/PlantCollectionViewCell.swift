3f13893599f0e5c2e2865a59d66130eae52a616a
Maram <mardhf9494@gmail.com>
2023-01-15 20:23:46 +0300

    plants collection setup

---
+++ GoldenLeaf/GoldenLeaf/Controllers/PlantCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlantCollectionViewCell.swift
+//  GoldenLeaf
+//
+//  Created by maram  on 22/06/1444 AH.
+//
+
+import UIKit
+
+class PlantCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgPlant: UIImageView!
+    
+    @IBOutlet weak var PlantName: UILabel!
+    
+}
