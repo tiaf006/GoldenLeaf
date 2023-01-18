e0ec4c4c432c0e2c9af4db0d6739b4bbddac57d2
Maram <mardhf9494@gmail.com>
2023-01-18 16:55:28 +0300

    location update

--- GoldenLeaf/GoldenLeaf/Controllers/LocationViewController.swift
+++ GoldenLeaf/GoldenLeaf/Controllers/LocationViewController.swift
@@ -7,37 +7,144 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class LocationViewController: UIViewController {
-
-    @IBOutlet weak var mapView: MKMapView!
+class LocationViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
+    var locationMangager = CLLocationManager()
+    var prevLocation: CLLocation? = nil
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let initalLoc = CLLocation(latitude: 24.774265, longitude: 46.738586)
         setStartingLocation(location: initalLoc, distance: 3000)
-        addAnnotation()
+
+        locationMangager.delegate = self
+        locationMangager.desiredAccuracy = kCLLocationAccuracyBest
+        locationMangager.allowsBackgroundLocationUpdates = true
+        mapView.delegate = self
+        if isLocationServiceEnabled() {
+            checkAuthorization()
+        }else{
+            showAlert(msg: "please enable location service")
+        }
+
+    }
+    // get location info
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let newLocation = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        if prevLocation == nil || prevLocation!.distance(from: newLocation) > 100 {
+            if prevLocation != nil {
+                print("distance: \(prevLocation!.distance(from: newLocation))")
+            }
+            getLocationInfo(location: newLocation)
+        }
+       
+    }
+    func getLocationInfo(location: CLLocation){
+        prevLocation = location
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location) { places, errorr in
+            guard let places = places?.first, errorr == nil else {return}
+            
+            print("----------------")
+            print("place name \(places.name ?? "no name to display")")
+            print("place country \(places.country ?? "no country to display")")
+            print("place country code  \(places.isoCountryCode ?? "no country code to display")")
+            print("place administrativeArea  \(places.administrativeArea ?? "no administrativeArea to display")")
+            print("place Locality \(places.locality ?? "no Locality to display")")
+            print("place PostalCode \(places.postalCode ?? "no PostalCode to display")")
+
+        }
     }
     
     func setStartingLocation(location: CLLocation, distance: CLLocationDistance){
         let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 24.774265, longitudinalMeters: 46.738586)
         mapView.setRegion(region, animated: true)
         
-        // lamited bondray
-//        mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
         
         let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 2150000)
         mapView.setCameraZoomRange(zoomRange, animated: true)
         
     }
-    func addAnnotation(){
-        let pin = MKPointAnnotation()
-        pin.coordinate = CLLocationCoordinate2D(latitude: 24.774265, longitude: 46.738586)
-        pin.title = "My Title"
-        pin.subtitle = "My Pin Subtitle"
-        mapView.addAnnotation(pin)
-        
+    func isLocationServiceEnabled() -> Bool{
+        return CLLocationManager.locationServicesEnabled()
     }
+    
+    
+    func checkAuthorization(){
+        switch locationMangager.authorizationStatus {
+        case .notDetermined:            // first time to open the app
+            locationMangager.requestAlwaysAuthorization()
+            break
+        case .authorizedWhenInUse:          // give the access ontime of use the app
+            locationMangager.requestAlwaysAuthorization()
+            locationMangager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+            break
+        case .authorizedAlways:            // While using the application and the background
+            locationMangager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+            break
+        case .denied:                      // denied the access
+            showAlert(msg: "Please authorize access to location")
+            break
+        case .restricted:                   // unable to access
+            showAlert(msg: " Authorize restricted")
+            break
+        default:
+            print(" default . .")
+            break
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("location \(location.coordinate)")
+            zoomToUserLocation(location: location)
+        }
+        locationMangager.stopUpdatingLocation()
+    }
+    func zoomToUserLocation(location: CLLocation){
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:            // first time to open the app
+            locationMangager.requestAlwaysAuthorization()
+            break
+        case .authorizedWhenInUse:          // give the access ontime of use the app
+            locationMangager.requestAlwaysAuthorization()
+            locationMangager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+            break
+        case .authorizedAlways:            // While using the application and the background
+            locationMangager.startUpdatingLocation()
+            mapView.showsUserLocation = true
+            break
+        case .denied:                      // denied the access
+            showAlert(msg: "Please authorize access to location")
+            break
+      
+        default:
+            print(" default . .")
+            break
+        }
+    }
+    
+    func showAlert(msg: String){
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "close", style: .default))
+        present(alert,animated: true,completion: nil)
+        alert.addAction(UIAlertAction(title: "setting", style: .default, handler: { actio in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        }))
+        present(alert,animated: true,completion: nil)
+    }
+
     
 }
